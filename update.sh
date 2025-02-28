be0ac64463f4acc45cd6828c8d520bc49a917fd0
G82ft <leonidkhvan123@gmail.com>
2025-02-28 18:15:28 +0300

    Fixed typo, finally working

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 cd ai-group-chat || exit
 git pull && docker buildx build . -t agc && docker rm -f agc
-docker run -v "$HOST"/agc/data:/app/data -v "$HOST"/agc/config:/app/config --name agc -itd agc
+docker run -v "$HOME"/agc/data:/app/data -v "$HOME"/agc/config:/app/config --name agc -itd agc
