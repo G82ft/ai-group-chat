97c1df7737e8b78cdee9af77610958566ea93538
G82ft <leonidkhvan123@gmail.com>
2025-02-28 17:25:03 +0300

    Testing update.sh

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
-VOLUME ["/app/config", "/app/data"]
 COPY . .
 
 ENTRYPOINT ["/bin/ash"]
--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 cd ai-group-chat || exit
 git pull && docker buildx build . -t agc && docker rm -f agc
-docker run -v "$HOME"/agc/data:/app/data -v "$HOME"/agc/config:/app/config --name agc -itd --rm agc
+docker run --mount type=shared,src="$HOME"/agc/data,dst=/app/data --name agc -itd --rm agc
