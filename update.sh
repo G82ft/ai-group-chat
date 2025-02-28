f1f572c36e1ee68fea9dd45f81aaf85415ccdb86
G82ft <leonidkhvan123@gmail.com>
2025-02-28 17:04:52 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 cd ai-group-chat || exit
-git pull && docker build . -t agc && docker rm -f agc
+git pull && docker buildx . -t agc && docker rm -f agc
 docker run -v "$HOME"/agc/data:/app/data -v "$HOME"/agc/config:/app/config --name agc -itd --rm agc
