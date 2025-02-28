9b0ec9c8b92d8f40f98bb3fba0e92218b53ff7cb
G82ft <leonidkhvan123@gmail.com>
2025-02-28 16:13:47 +0300

    Testing update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 cd ai-group-chat || exit
 git pull && docker build . -t agc && docker rm -f agc
-docker run --mount type=bind,src=~/agc/data,dst=/app/data,bind-propagation=shared -v ~/agc/config:/app/config --name agc agc
+docker run --mount type=bind,src="$HOME"/agc/data,dst=/app/data,bind-propagation=shared -v /root/agc/config:/app/config --name agc agc
