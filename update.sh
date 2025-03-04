83c010482f9ba37787db132798f7d452983c102c
G82ft <leonidkhvan123@gmail.com>
2025-03-05 00:01:31 +0300

    Fix typo

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 cd ai-group-chat || exit
 git pull && docker buildx build . -t agc && docker rm -f agc
-docker run -v /car/lib/agc/chats:/app/chats -v car/lib/agc/config:/app/config --name agc -itd agc
+docker run -v /var/lib/agc/chats:/app/chats -v /var/lib/agc/config:/app/config --name agc -itd agc
